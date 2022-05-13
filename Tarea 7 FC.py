31e809bf85fee3643b6c7c8593b54c2ad79e939b
Jorge0801 <105502273+Jorge0801@users.noreply.github.com>
2022-05-13 08:48:05 -0600

    Update Tarea 7 FC.py

--- Tarea 7 FC.py
+++ Tarea 7 FC.py
@@ -5,13 +5,4 @@ Created on Fri May 13 08:23:20 2022
 @author: Jorge
 """
 
-Hola mundo 
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 13 08:23:20 2022
-
-@author: Jorge
-"""
-
-print("si")
+Hola mundo
